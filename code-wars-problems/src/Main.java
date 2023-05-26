76707e2517bf58fcbfa00f84cf68648fa5d7f5bd
Archie <110494298+archie122@users.noreply.github.com>
2023-05-25 20:20:51 -0600

    Issue Fixed
    There was a problem, which has now been resolved

--- code-wars-problems/src/Main.java
+++ code-wars-problems/src/Main.java
@@ -3,8 +3,8 @@ import java.util.List;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String args[]){
-        System.out.println(DRoot(942));
+    public static void main(String[] args){
+        System.out.println(DRoot(2134));
     }
 
     public static int DRoot(int x) {
--- code-wars-problems/src/client.java
+++
@@ -1,7 +0,0 @@
-
-public class client{
-    public static void main(String args[]){
-        Problem1 test = new Problem1();
-        System.out.println(test.DRoot(10));
-    }
-}
