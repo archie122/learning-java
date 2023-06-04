18590edd652e629684a40addb02957396c16a8ea
Archie <110494298+archie122@users.noreply.github.com>
2023-06-04 09:10:27 -0600

    Update Problem14.java

--- code-wars-problems/src/Problem14.java
+++ code-wars-problems/src/Problem14.java
@@ -1,9 +1,5 @@
 public class Problem14 {
     public static String repeatStr(final int repeat, final String string){
-        String result = "";
-        for(int i = 0; i < repeat; i++){
-            result += string;
-        }
-        return result;
+        return String.valueOf(string).repeat(repeat);
     }
 }
