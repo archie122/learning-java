e3db4a98dc3a25d0d36d1834413941311fbdf3a3
Archie <110494298+archie122@users.noreply.github.com>
2023-06-04 07:42:07 -0600

    Kinda works

--- code-wars-problems/src/Problem13.java
+++ code-wars-problems/src/Problem13.java
@@ -1,42 +1,29 @@
+import java.util.*;
 public class Problem13 {
     public int romanToInt(String s) {
         int result = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (i < s.length() - 1 && s.charAt(i) == 'I' && s.charAt(i + 1) == 'V') {
-                result += 4;
-                i++;
-            } else if (i < s.length() - 1 && s.charAt(i) == 'I' && s.charAt(i + 1) == 'X') {
-                result += 9;
-                i++;
-            } else if (i < s.length() - 1 && s.charAt(i) == 'X' && s.charAt(i + 1) == 'L') {
-                result += 40;
-                i++;
-            } else if (i < s.length() - 1 && s.charAt(i) == 'X' && s.charAt(i + 1) == 'C') {
-                result += 90;
-                i++;
-            } else if (i < s.length() - 1 && s.charAt(i) == 'C' && s.charAt(i + 1) == 'D') {
-                result += 400;
-                i++;
-            } else if (i < s.length() - 1 && s.charAt(i) == 'C' && s.charAt(i + 1) == 'M') {
-                result += 900;
-                i++;
-            } else if (s.charAt(i) == 'I') {
-                result += 1;
-            } else if (s.charAt(i) == 'V') {
-                result += 5;
-            } else if (s.charAt(i) == 'X') {
-                result += 10;
-            } else if (s.charAt(i) == 'L') {
-                result += 50;
-            } else if (s.charAt(i) == 'C') {
-                result += 100;
-            } else if (s.charAt(i) == 'D') {
-                result += 500;
-            } else if (s.charAt(i) == 'M') {
-                result += 1000;
-            }
+        Map<String, Integer> map = new LinkedHashMap<>();
+        map.put("I", 1);
+        map.put("IV", 4);
+        map.put("V", 5);
+        map.put("IX", 9);
+        map.put("X", 10);
+        map.put("XL", 40);
+        map.put("L", 50);
+        map.put("XC", 90);
+        map.put("C", 100);
+        map.put("CD", 400);
+        map.put("D", 500);
+        map.put("CM", 900);
+        map.put("M", 1000);
+
+        for (int i = 0; i < s.length(); i++){
+            result += map.get(s.substring(i, i+1));
         }
+
+
+
+
         return result;
     }
-
 }
